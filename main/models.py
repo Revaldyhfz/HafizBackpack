e38a901500ba9c28d40373d6550920874673bc47
Revaldyhfz <revaldyhafiz16@gmail.com>
2023-09-21 10:58:49 +0700

    Assignment 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -6,3 +7,4 @@ class Product(models.Model):
     amount = models.IntegerField()
     description = models.TextField()
     fruit_type = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
